8c6b694dd74626ae2d82b04bc5dcdb489dcf56b7
Yoann Losbar <yoann.losbar@gmail.com>
2012-02-08 18:55:20 +0100

    Adding AutoSave feature

---
+++ server/model.php
@@ -0,0 +1,47 @@
+<?php
+
+switch($_SERVER['REQUEST_METHOD']){
+    case 'POST':
+        save();
+    break;
+    case 'GET':
+        fetch();
+    break;
+    case 'PUT':
+        save();
+    break;
+    case 'DELETE':
+        destroy();
+    break;
+}
+
+/**
+ * Save model.
+**/
+function save() {
+    $values = json_decode(file_get_contents('php://input'), true);
+    $handle = fopen($values[id].".json","w");
+    fwrite($handle, json_encode($values));
+    fclose($handle);
+}
+
+/**
+* Fetch model.
+**/
+function fetch() {
+    $jsonFile = $_GET['id'].".json";
+    $fh = fopen($jsonFile, 'r');
+    $jsonData = fread($fh, filesize($jsonFile));
+    fclose($fh);
+    echo $jsonData;
+}
+
+/**
+* Destroy model.
+**/
+function destroy() {
+    $jsonFile = $_GET['id'].".json";
+    unlink($jsonFile);
+}
+?>
+
