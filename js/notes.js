15892a925b6cccdf91e52d0720629c71eed737e2
unknown <Marjorie@Marjorie-PC.(none)>
2012-03-09 16:41:56 +0100

    Adding new methods for the history Adding a History Collection method to load history list method to display the browsing history interface method to hide the browsing history interface method to back up the loaded history

--- js/notes.js
+++ js/notes.js
@@ -19,9 +19,19 @@ $(function() {
             };
         }
     });
-
+	
+	/** History collection */
+    window.NotesHistoryCollection = Backbone.Collection.extend({
+        model : NotesModel,
+        url : 'server/history.php',
+		
+		initialize: function(){
+			console.log('NotesHistoryColllection Constructor');
+		}
+    });
+	
     /* We must set an id to store locally a single model. It is different than collections */
-    var Notes = new NotesModel({id: 1});
+    Notes = new NotesModel({id: 1});
 
     /* Here is our View */
     window.NotesView = Backbone.View.extend({
@@ -33,16 +43,23 @@ $(function() {
 
         /* Our main element */
         el: $("#BalloonNotesContainer"),
+		
+		/** history list */
+        list: $('#history-list'),
 
         /* Autosave frequency in seconds */
         autosaveInterval: 20,
 
         events: {
-            "keyup":                  "editAndSave",
-            "focus #BalloonNotes":    "hasFocus",
-            "click #notes-clear":     "reset",
-            "click #notes-save":      "autoSave",
-            "click #send":            "sendMail",
+            "keyup":                  	"editAndSave",
+            "focus #BalloonNotes":    	"hasFocus",
+            "click #notes-clear":     	"reset",
+            "click #notes-save":      	"autoSave",
+            "click #send":            	"sendMail",
+			"click #history-list a":  	"loadHistory",
+			"click #browse-history":  	"browseHistory",
+			"click #hide-history":		"hideHistory",
+			"click #load-history":		"backUpHistory"
         },
 
         /**
@@ -54,14 +71,23 @@ $(function() {
             /* Instanciate auto-save, must give context to setInterval func */
             setInterval((function(self) {
                 return function() {
-                    if (!this.$("#notes-save").hasClass('disabled') && Notes.get("words") !== 0) {
+                    if (!self.$("#notes-save").hasClass('disabled') && Notes.get("words") !== 0) {
                         self.autoSave();
                     }
-                }})(this),
-            this.autosaveInterval*1000);
+                }})(self),
+            self.autosaveInterval*1000);
+			
+			/** Instanciate history collection */
+            self.history = new NotesHistoryCollection();
+			
+			/** init underscore template for history list, fetch it and render it */
+            self.template = _.template($('#history-template').html());
+			self.history.fetch({location:'remote',success: function(){
+				self.renderList();
+			}});
 
             /* Check for local and remote storage then render */
-            this.initFetch();
+            self.initFetch();
         },
 
         /**
@@ -76,6 +102,17 @@ $(function() {
 
             /* Display the number of words below notes */
             this.displayNumberWords(Notes.get("words"));
+        },
+		
+		/**
+         * Render notes list
+         */
+        renderList : function() {
+            if (this.history.length > 0) {
+                var content = this.template({history : this.history.toJSON()});
+                this.list.html(content);
+            }
+            return this;
         },
 
         /**
@@ -98,14 +135,35 @@ $(function() {
                 Notes.save({"lastSave": time},{
                     'location' : 'remote',
                     success: function() {
+						self.history.fetch({location: 'remote', success: function() {
+                            self.renderList();
+                        }});
                         self.saveButton('saved', time);
                         Notes.save({"lastSave": time});
                     }
                 });
-            }
-
+            }			
             return false;
         }, 800),
+		
+		/**
+         * Load auto save interval
+         */
+        setAutoSaveInterval: function(self) {
+            /* Fetch notes from localStorage, must give context& */
+            if (undefined != this.interval) {
+                clearInterval(this.interval);
+            }
+            
+            this.interval = setInterval((function(self) {
+                return function() {
+                    self.saveButton('saving');
+                    self.autoSave();
+                }})(this),
+            this.autosaveInterval*1000);
+            
+            return this;
+        },
 
         /**
         *   On page loading, compare what we have in localStorage and in distant storage, then choose
@@ -170,7 +228,64 @@ $(function() {
                 button.text('Save | '+save);
             }
         },
+		
+		/**
+		 * Load data from one history model and add a class to the history button
+		 */
+		loadHistory: function(e) {
+			e.preventDefault();
+			var id = $(e.target).attr('data-id'),
+			histo = this.history.get(id);
 
+			$('#BalloonHistory').text(histo.get('notes')).attr({
+				'data-words': histo.get('words'),
+			});
+			$('#history-list a.btn-info').removeClass('btn-info');
+			$(e.target).addClass('btn-info');
+        },
+		
+		/**
+		*
+		**/
+		backUpHistory: function(){
+			var notesFromHistory	= $('#BalloonHistory').text();
+			var wordsFromHistory	= $('#BalloonHistory').attr('data-words');
+			
+			//renew interval to avoid saving directly after loading history
+            this.setAutoSaveInterval(this);
+			
+			Notes.set({
+				notes	: notesFromHistory,
+				words	: wordsFromHistory
+			});
+			this.render();
+			
+			this.hideHistory();
+		},
+		
+		/**
+		*
+		**/
+		browseHistory: function(){
+			$('#BalloonNotes').animate({height:'50%'}, 300,function(){
+				$('#history').fadeIn(300);
+				$('#browse-history').attr('id','hide-history').html('Hide history');
+			});
+		},
+		
+		/**
+		*
+		**/
+		hideHistory: function(){
+			$('#history').fadeOut(300,function(){
+				$('#BalloonNotes').animate({height:'100%'}, 300);
+				$('#hide-history').attr('id','browse-history').html('Browse history');
+			});	
+		},
+
+		/**
+		*	Return last save time to be displayed
+		**/
         getTime: function(time){
             var date = new Date(time);
             return 'Last save : ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
